20c7f4c61ecbbbbb8d3ad6e90ca0b0105f1c666a
stavrosvl7 <stavrosvl7@gmail.com>
2025-05-05 15:31:12 +0300

    Add Filter Based on contract Address for Logs

--- node/pkg/watchers/aztec/block_processor.go
+++ node/pkg/watchers/aztec/block_processor.go
@@ -70,6 +70,14 @@ func (w *Watcher) processBlockLogs(ctx context.Context, blockNumber int, blockIn
 
 	// Process each log
 	for _, log := range logs {
+		// Skip logs that don't match our contract address
+		if log.Log.ContractAddress != w.config.ContractAddress {
+			w.logger.Debug("Skipping log from different contract",
+				zap.String("expected", w.config.ContractAddress),
+				zap.String("actual", log.Log.ContractAddress))
+			continue
+		}
+
 		// Get the correct transaction hash for this log
 		txHash := "0x0" // Default if we can't find the right transaction
 
